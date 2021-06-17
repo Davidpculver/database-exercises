0fb8a7f50a6c799cfb0380d046c9031de129a153
David Culver <culver.david.p@gmail.com>
2021-06-17 16:06:21 -0500

    completed sql tables exercises created table albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,14 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales DOUBLE NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
