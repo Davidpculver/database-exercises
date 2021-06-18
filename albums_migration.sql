ced30cb1b1511520aa802e07a5e9525c3f6e02ae
David Culver <culver.david.p@gmail.com>
2021-06-18 11:43:19 -0500

    finished first part of basic statments exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,6 +3,7 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums
+-- only column that has to been NOT NULL is the id since it's the primary key
 (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
@@ -12,3 +13,7 @@ CREATE TABLE albums
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- how to add to our query. Values have to correspond to our table data column
+-- INSERT INTO albums(artist, name, release_date, sales, genre)
+-- VALUES('Taylor Swift', 'Fearless', 2008, 10000000, 'Country');
