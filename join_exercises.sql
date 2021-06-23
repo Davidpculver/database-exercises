a4201fbb9f6e1d9b9642b08f9a6b421e8d0de379
David Culver <culver.david.p@gmail.com>
2021-06-23 12:44:50 -0500

    join exercises

---
+++ join_exercises.sql
@@ -0,0 +1,57 @@
+USE join_test_db;
+INSERT INTO users (name, email, role_id)
+VALUES ('David', 'david@codeup.com', null),
+       ('Jackie', 'jackie@codeup.com', 2),
+       ('Winston', 'winston@dog.com', 2),
+       ('Bob', 'bob@humpback.com', 2);
+
+# Select all current managers
+USE employees;
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees e
+         JOIN dept_manager dm on e.emp_no = dm.emp_no
+         JOIN departments d on dm.dept_no = d.dept_no
+WHERE dm.to_date > now()
+limit 10;
+
+# INSTRUCTOR EXAMPLE
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees e
+         JOIN dept_manager dm on e.emp_no = dm.emp_no
+         JOIN departments d on dm.dept_no = d.dept_no
+WHERE dm.to_date LIKE '9%';
+
+
+
+
+# Select current managers that are female
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees e
+         JOIN dept_manager dm on e.emp_no = dm.emp_no
+         JOIN departments d on dm.dept_no = d.dept_no
+WHERE dm.to_date > now()
+  AND e.gender = 'f'
+limit 10;
+
+# Current titles of employees working in cust service dept
+# Instructor example
+SELECT t.title, COUNT(t.title) AS Total
+FROM titles as t
+         JOIN dept_emp as de
+              ON de.emp_no = t.emp_no
+WHERE de.dept_no = 'd009' AND YEAR(t.to_date) = '9999' AND Year(de.to_date) = '9999'
+GROUP BY t.title;
+
+
+
+
+# Attempting to get current salaries for all managers
+SELECT d.dept_name                            AS 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
+       s.salary                               AS Salary
+FROM employees e
+         JOIN dept_manager dm on e.emp_no = dm.emp_no
+         JOIN departments d on dm.dept_no = d.dept_no
+         JOIN salaries s on s.emp_no = e.emp_no
+WHERE dm.to_date > now()
+limit 10;
--- joins_lec.sql
+++ joins_lec.sql
@@ -50,10 +50,44 @@ VALUES ('Olivia', 29),
 
 # Using a table alias: Prevents having to write out entire table name with dot notation.
 # FROM persons p = generates alias for persons table, albums a = generates alias for albums table.
-SELECT p.first_name, a.name FROM persons p JOIN albums a ON p.album_id = a.id;
+# Regular join (Inner join)
+SELECT p.first_name, a.name
+FROM persons p
+         JOIN albums a ON p.album_id = a.id;
 
+# LEFT join (outer join)
+# Making albums left table: on left side of left join keyword. Gibe me all the rows in the left table whether or not they have a match for the criteria. Order set by after Select, but use Albums as left table.
+# Below returns null where there is not a match for name next to album
+SELECT p.first_name, a.name
+FROM albums a
+         LEFT JOIN persons p on a.id = p.album_id;
 
+# RIGHT JOIN - can always be refactored to left join so not used often
+SELECT p.first_name, a.name
+FROM persons p
+         RIGHT JOIN albums a on a.id = p.album_id;
 
+SELECT p.first_name, a.name
+FROM albums a
+         RIGHT JOIN persons p on a.id = p.album_id;
 
+# outer join will return every row of the table while inner join only returns the rows where there is a match - FUNDAMENTAL DIFFERENCE between the two.
+
+# junction tables / associative tables / join tables
+# atomic values make querying faster
+
+# creating table so person can have multiple preferences
+CREATE TABLE preferences
+(
+    person_id INT NOT NULL,
+    album_id  INT NOT NULL
+);
+
+INSERT INTO preferences (person_id, album_id) VALUES (1, 12), (1, 5), (1, 22), (1, 29), (2, 1), (2, 31), (2, 30), (3, 11), (3, 26), (3, 25);
+
+# to connect to our albums table
+SELECT p.first_name AS name, a.name AS album FROM persons p JOIN preferences pf ON p.person_id = pf.person_id JOIN albums a ON pf.album_id = a.id;
+
+# YOU can circle back to a table you've already joined from, give it a different alias and join to it again.
 
 
